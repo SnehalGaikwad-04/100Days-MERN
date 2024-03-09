6da5ba1f34f74327c28c84592fef33ee7ba42c83
Snehal Gaikwad <161202223+SnehalGaikwad-04@users.noreply.github.com>
2024-03-09 17:54:41 +0530

    Update basics.js

--- Day-10 JS pt1/basics.js
+++ Day-10 JS pt1/basics.js
@@ -1,1 +1,13 @@
-//write code here
+let num = 42; // Declaring a variable 'num' with the number data type and assigning it the value 42
+let str = "Hello, world!"; // Declaring a variable 'str' with the string data type and assigning it the value "Hello, world!"
+let isTrue = true; // Declaring a variable 'isTrue' with the boolean data type and assigning it the value true
+let arr = [1, 2, 3, 4, 5]; // Declaring a variable 'arr' with the array data type and assigning it an array of numbers
+let undefinedVar; // Declaring a variable 'undefinedVar' without assigning any value, resulting in its value being 'undefined'
+let nullVar = null; // Declaring a variable 'nullVar' and explicitly assigning it the value null
+
+//diff number types
+let integerNum = 42; // Declaring an integer variable 'integerNum' with the value 42
+let floatNum = 3.14; // Declaring a floating-point variable 'floatNum' with the value 3.14
+let scientificNum = 1.2e3; // Declaring a variable 'scientificNum' using scientific notation (1.2 * 10^3)
+let nanNum = NaN; // Declaring a variable 'nanNum' with the value NaN
+let infinityNum = Infinity; // Declaring a variable 'infinityNum' with the value Infinity
