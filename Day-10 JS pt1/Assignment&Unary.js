5f15603b0afb485a323b44dcc92e4a768b0ddd72
Snehal Gaikwad <161202223+SnehalGaikwad-04@users.noreply.github.com>
2024-03-09 18:01:14 +0530

    Create Assignment&Unary.js

---
+++ Day-10 JS pt1/Assignment&Unary.js
@@ -0,0 +1,9 @@
+let x = 5; // Assigning the value 5 to the variable x
+
+let c = -3;
+let positiveC = +c; // Unary plus operator; converts c to a positive number
+// positiveC will be 3
+let d = 5;
+let negativeD = -d; // Unary minus operator; negates the value of d
+// negativeD will be -5
+
