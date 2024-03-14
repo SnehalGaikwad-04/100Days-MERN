162d3fb089afd35a2b4a092d511dd25f74ccbe05
Snehal Gaikwad <161202223+SnehalGaikwad-04@users.noreply.github.com>
2024-03-14 19:45:13 +0530

    Add files via upload

---
+++ Day-15 JS pt3/ArrMethods.js
@@ -0,0 +1,7 @@
+let flavours=['chocolate','strawberry','pistachio','blackcurrent'];
+
+//practice set
+let months=['jan','july','mar','aug'];
+let months2=['sep','oct','nov','dec'];
+//methods on array
+let students=[90,98,94,92,100];
