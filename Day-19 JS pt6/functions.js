c8ac6c8be57e60b0f2950463aeff14a8d4d72795
Snehal Gaikwad <161202223+SnehalGaikwad-04@users.noreply.github.com>
2024-03-21 17:10:45 +0530

    Update functions.js

--- Day-19 JS pt6/functions.js
+++ Day-19 JS pt6/functions.js
@@ -27,3 +27,51 @@ function unique(str){
 
 console.log('unique chars= '+unique(string));
 
+
+//longest country name as output
+let country=['India','Germany','United States of America'];
+console.log('longest country name= ')
+console.log(longest(country));
+
+function longest(country){
+    let ansidx=0;
+    for(let i=0; i<country.length; i++){
+        let anslen=country[ansidx].length;
+        let currlen=country[i].length;
+        if(currlen>anslen){
+            ansidx=i;
+        }
+    }
+    return country[ansidx];
+}
+
+
+//count the number of vowels in string
+let string2='my name is snehal';
+console.log('count of vowels in "'+string2+'" is '+counting(string2));
+
+function counting(str){
+    let vowel=['a','e','i','o','u'];
+    let count=0;
+
+    for(let i=0; i<str.length; i++){
+        for(let j=0; j<vowel.length; j++){
+            if(str[i]==vowel[j]){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+
+//generate a random number within a range (start,end)
+start=50;
+end=90;
+console.log('random number between '+start+' & '+end+' =  '+generate(start,end));
+
+function generate(x,y){
+    let diff=y-x;
+    let numbers=Math.floor(Math.random()*diff)+x;
+    return numbers;
+}
